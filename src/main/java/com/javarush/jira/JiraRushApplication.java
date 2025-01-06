09ba86513a8de8564df152bd5596e8891f2bbb51
Stoliar Kiril <kiss200486@mail.ru>
2025-01-06 14:53:01 +0300

    Update env-file, application.yaml, docker-compose.yml, pom.xml

--- src/main/java/com/javarush/jira/JiraRushApplication.java
+++ src/main/java/com/javarush/jira/JiraRushApplication.java
@@ -6,12 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
 @EnableCaching
 public class JiraRushApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
         SpringApplication.run(JiraRushApplication.class, args);
     }
 }
